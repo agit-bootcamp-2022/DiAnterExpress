df9a2543f1105ba7b084b81a6d487efc625e35a3
irfhandomahendra <irfhandomahendra@gmail.com>
2022-01-27 10:45:38 +0700

    rebase, fix some issue

---
+++ Profiles/ShipmentProfile.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DiAnterExpress.Dtos;
+using DiAnterExpress.Models;
+using NetTopologySuite.Geometries;
+
+namespace DiAnterExpress.Profiles
+{
+    public class ShipmentProfile : Profile
+    {
+        public ShipmentProfile()
+        {
+            CreateMap<Shipment, DtoShipmentOutput>()
+            .ForMember(dst => dst.SenderAddress,
+                opt => opt.MapFrom(src => new location { Lat = src.SenderAddress.X, Long = src.SenderAddress.Y }))
+            .ForMember(dst => dst.ReceiverAddress,
+                opt => opt.MapFrom(src => new location { Lat = src.ReceiverAddress.X, Long = src.ReceiverAddress.Y }))
+            .ForMember(dst => dst.Status,
+                opt => opt.MapFrom(src => (Status)src.Status));
+        }
+    }
+}
---
+++ Profiles/StatusProfile.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DiAnterExpress.Dtos;
+using DiAnterExpress.Models;
+
+namespace DiAnterExpress.Profiles
+{
+    public class StatusProfile : Profile
+    {
+        public StatusProfile()
+        {
+            CreateMap<Shipment, DtoStatus>()
+            .ForMember(dst => dst.Status,
+                opt => opt.MapFrom(src => (Status)src.Status));
+        }
+        
+    }
+}
