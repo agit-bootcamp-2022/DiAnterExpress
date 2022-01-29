37579f1a2b2d1162fa8b4f628a084928dda5a815
Hilman Abdan <hilmanabdan28@gmail.com>
2022-01-29 17:37:50 +0700

    Add swagger to prod env

--- Startup.cs
+++ Startup.cs
@@ -116,12 +116,9 @@ namespace DiAnterExpress
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiAnterExpress v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiAnterExpress v1"));
 
             app.UseHttpsRedirection();
 
